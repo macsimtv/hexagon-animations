2f675723eceac203b0bb6da274a689bf763d1a5d
macsimtv <maximelecouturier@gmail.com>
2022-08-10 11:03:39 +0200

    [V2] Class

--- hexagon.class.js
+++ hexagon.class.js
@@ -1,23 +1,45 @@
 class Hexagon {
-    constructor(x, y, size) {
+    constructor(x, y, size, frame, app) {
         this.x = x;
         this.y = y;
         this.size = size;
+
         this.hexagonPointsCordinates = [];
         this.circles = [];
-        this.isAnimated = false;
+
+        this.isAnimated = true;
         this.isHovered = false;
         this.isClicked = false;
+
+        this.hexagon;
+        this.container;
+
+        this.frame = frame;
+        this.app = app;
+
+        this._init();
+    }
+
+    _init() {
+
+        this.container();
+        this.draw();
+        this.app.ticker.add(this.animate.bind(this));
+    }
+
+    container() {
+        // Create container
+        this.container = new PIXI.Container();
+        this.frame.addChild(this.container);
     }
 
     draw() {
-        for (let side = 0; side < 7; side++) {
+        for (let side = 0; side < 6; side++) {
             this.hexagonPointsCordinates[side] = new PIXI.Point(this.x + this.size * Math.cos(side * 2 * Math.PI / 6), this.y + this.size * Math.sin(side * 2 * Math.PI / 6));
         }
 
         this.hexagon = new PIXI.Graphics();
-        this.hexagon.beginFill(0xffffff);
-        this.hexagon.lineStyle(1, 0xFFFFFF);
+        this.hexagon.lineStyle(1, 0x7a736a);
         this.hexagon.drawPolygon(this.hexagonPointsCordinates);
         this.hexagon.endFill();
 
@@ -26,6 +48,9 @@ class Hexagon {
 
         this.hexagon.buttonMode = true;
         this.hexagon.interactive = true;
+
+        this.drawCircles();
+        this.addToFrame();
     }
 
     drawCircles() {
@@ -37,10 +62,29 @@ class Hexagon {
 
             // Draw circle
             let circle = new PIXI.Graphics();
-            circle.beginFill(0xff0000);
-            circle.drawCircle(pointCoordinate.x, pointCoordinate.y, 5);
+            circle.beginFill(0xbbbab7);
+            circle.drawCircle(pointCoordinate.x, pointCoordinate.y, 2);
             circle.endFill();
+
             this.circles.push(circle);
         }
-    }        
+    }  
+    
+    addToFrame() {
+        this.container.addChild(this.hexagon);
+
+        for (let circle of this.circles) {
+            this.container.addChild(circle);
+        }
+    }
+
+    animate() {
+        if (!this.isAnimated) {
+            return;
+        }
+
+        this.container.x += 0.1;
+        this.container.lineStyle(1, 0x7a736a);
+        this.container.drawPolygon(this.hexagonPointsCordinates);
+    }
 }
