4bf1d23f97a39c8e4c85289caf4f2cdf7edaaa21
macsimtv <maximelecouturier@gmail.com>
2022-08-17 16:04:52 +0200

    v3

--- app.js
+++ app.js
@@ -13,25 +13,58 @@ const app = new PIXI.Application({
     height: _h / ratio,
 });
 
-// Container
-const container = new PIXI.Container();
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+// Resize
+window.addEventListener('resize', () => {
+    _w = window.innerWidth;
+    _h = window.innerHeight;
+    app.renderer.resize(_w / ratio, _h / ratio);
+});
 
-const x = 0;
-const y = 0;
+const x = app.screen.width / 2;
+const y = app.screen.height / 2;
 
 const size = 50;
 
 let hexagons = [];
 
-for(let i = 0; i < 2; i++) {
-    hexagons.push(new Hexagon(x + (i * (size * 2)), y, size, container, app));
+let column = app.screen.width / (size);
+let row = app.screen.height / (size * 2);
+
+// Create hexagon grid
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        if(i % 2 == 0) {
+            hexagons.push(new Hexagon(x + (i * size), y + (j * size * 3), size, app));
+        }
+        else {
+            hexagons.push(new Hexagon(x + (i * size), y + (j * size * 3) + size * 1.5, size, app));
+        }
+    }
 }
 
-console.log(hexagons);
+// if(i % 2 === 0) {
+//     hexagons.push(new Hexagon(i, y + (j * (size * 3)), size, app));
+// } else {
+//     hexagons.push(new Hexagon(i + size, y + (size * 1.5) + (j * (size * 3)), size, app));
+// }
 
-app.stage.addChild(container);
+hexagons[0].makeCursor();
+
+// for(let i = 0; i < 3; i++) {
+//     hexagons.push(new Hexagon(x, y + (i * (size * 3)), size, app));
+// }
+
+// for(let i = 0; i < 3; i++) {
+//     hexagons.push(new Hexagon(x + size, y + (size * 1.5) + (i * (size * 3)), size, app));
+// }
+
+// for(let i = 0; i < 4; i++) {
+//     hexagons.push(new Hexagon(x + ((size * 2) - (size / 2)), y + (i * (size * 2 - (size / 4))) + size, size, app));
+// }
+
+// for(let i = 0; i < 4; i++) {
+//     hexagons.push(new Hexagon(x + (i * (size * 2 - (size / 4))) + ((size / 2) + (size / 2)), y + ((size / 2) + (size / 2)), size, app));
+// }
 
 // Animation
 app.ticker.add(animate);
--- hexagon.class.js
+++ hexagon.class.js
@@ -1,60 +1,149 @@
 class Hexagon {
-    constructor(x, y, size, frame, app) {
+    constructor(x, y, size, app) {
         this.x = x;
         this.y = y;
         this.size = size;
 
-        this.hexagonPointsCordinates = [];
+        this.polygonPointsCordinates = [];
         this.circles = [];
 
-        this.isAnimated = true;
+        this.isAnimated = false;
         this.isHovered = false;
         this.isClicked = false;
 
-        this.hexagon;
         this.container;
 
-        this.frame = frame;
+        this.hexagonContainer;
+        this.hexagon;
+        this.strokes = [];
+        this.strokesContainer;
+
+
+        this.cursor;
+        this.cursorEvent;
+
         this.app = app;
 
         this._init();
     }
 
     _init() {
-
         this.container();
         this.draw();
         this.app.ticker.add(this.animate.bind(this));
-    }
+        this.isAnimated = true;
 
+    }
+    
     container() {
         // Create container
         this.container = new PIXI.Container();
-        this.frame.addChild(this.container);
+        this.container.sortableChildren = true;
+        
+        this.hexagonContainer = new PIXI.Container();
+        this.hexagonContainer.zIndex = 5;
+        this.container.addChild(this.hexagonContainer);
+        
+        this.strokesContainer = new PIXI.Container();
+        this.strokesContainer.zIndex = 2;
+        this.container.addChild(this.strokesContainer);
+        
+        this.app.stage.addChild(this.container)
+        console.log('Yo')
     }
 
     draw() {
         for (let side = 0; side < 6; side++) {
-            this.hexagonPointsCordinates[side] = new PIXI.Point(this.x + this.size * Math.cos(side * 2 * Math.PI / 6), this.y + this.size * Math.sin(side * 2 * Math.PI / 6));
+
+            let size = this.size;
+
+            var angle_deg = 60 * side - 30
+            var angle_rad = Math.PI / 180 * angle_deg
+
+            this.polygonPointsCordinates[side] = new PIXI.Point(0 + size * Math.cos(angle_rad), 0 + size * Math.sin(angle_rad))
         }
 
         this.hexagon = new PIXI.Graphics();
+        this.hexagon.beginFill(0x00000000);
         this.hexagon.lineStyle(1, 0x7a736a);
-        this.hexagon.drawPolygon(this.hexagonPointsCordinates);
+        this.hexagon.drawPolygon(this.polygonPointsCordinates);
         this.hexagon.endFill();
-
-        this.hexagon.x = this.x;
-        this.hexagon.y = this.y;
 
         this.hexagon.buttonMode = true;
         this.hexagon.interactive = true;
 
+        this.hexagon.zIndex = 1;
+
+        this.hexagon.on('mouseover', (e) => {
+            this.isHovered = true;
+            this.cursorEvent = e;
+        });
+
+        this.hexagon.on('mouseout', (e) => {
+            this.isHovered = false;
+            this.cursorEvent = e;
+        });
+
         this.drawCircles();
-        this.addToFrame();
+        this.drawBars();
+        this.addToFrame.bind(this)();
+    }
+
+    makeCursor() {
+        this.cursor = new PIXI.Graphics();
+        this.cursor.moveTo(0, 0);
+
+        let canvas = document.createElement('canvas');
+        canvas.width  = 200;
+        canvas.height = 200;
+        let ctx = canvas.getContext('2d');
+        let gradient = ctx.createLinearGradient(0, 0, 0, 50);
+        gradient.addColorStop(0, "#D3872A");
+        gradient.addColorStop(1, "#CFB732");
+        ctx.fillStyle = gradient;
+        this.cursor = new PIXI.Sprite(PIXI.Texture.from("angryimg.png"));
+
+        // this.cursor.drawCircle(0, 0, 100);
+
+        this.cursor.zIndex = 100;
+        // Center cursor
+
+        this.app.stage.addChild(this.cursor);
+        this.app.stage.interactive = true;
+
+        this.app.stage.on('mousemove', (e) => {
+            this.cursor.position.x = e.data.global.x - this.cursor.width / 2;
+            this.cursor.position.y = e.data.global.y - this.cursor.height / 2;
+        });
+
+        this.app.stage.mask = this.cursor;
+    }
+
+    drawBars() {
+        this.strokes = [];
+
+        for (let corner of this.polygonPointsCordinates) {            
+            let pointCoordinate = {
+                x: corner.x,
+                y: corner.y
+            }
+
+            // Draw stroke
+            let stroke = new PIXI.Graphics();
+            stroke.lineStyle(1, 0x7a736a);
+            stroke.moveTo(this.hexagonContainer.position.x, this.hexagonContainer.position.y);
+            stroke.lineTo(pointCoordinate.x, pointCoordinate.y);
+            stroke.zIndex = 2;
+            this.strokes.push(stroke);
+        }
+
+        for (let stroke of this.strokes) {
+            this.strokesContainer.addChild(stroke);
+        }
     }
 
     drawCircles() {
-        for (let corner of this.hexagonPointsCordinates) {
+        for (let corner of this.polygonPointsCordinates) {
             let pointCoordinate = {
                 x: corner.x,
                 y: corner.y
@@ -66,21 +155,57 @@ class Hexagon {
             circle.drawCircle(pointCoordinate.x, pointCoordinate.y, 2);
             circle.endFill();
 
+            circle.zIndex = 2;
+
             this.circles.push(circle);
         }
     }  
     
     addToFrame() {
-        this.container.addChild(this.hexagon);
+        this.hexagonContainer.addChild(this.hexagon);
 
         for (let circle of this.circles) {
-            this.container.addChild(circle);
+            this.hexagonContainer.addChild(circle);
         }
+
+        this.container.x = this.x;
+        this.container.y = this.y;
     }
 
     animate() {
-        if (!this.isAnimated) {
-            return;
+        console.log(this.hexagonContainer.width, this.hexagonContainer.height);
+
+        if(this.isHovered) {
+            this.isAnimated = true;
+            TweenMax.to(this.hexagonContainer.scale, 1, { x: 0.50, y: 0.50, ease: Power2.easeOut });
+            TweenMax.to(this.hexagonContainer.position, .5, { x: this.cursorEvent.data.global.x - this.container.x, y: this.cursorEvent.data.global.y - this.container.y, ease: Power2.easeOut });
+
+            for (let stroke of this.strokes) {
+                stroke.clear();
+            }
+
+            this.drawBars.bind(this)();
+
+            if(this.hexagonContainer.position.x > this.hexagonContainer.width / 2 + 5 || this.hexagonContainer.position.x < -this.hexagonContainer.width / 2 - 5 || this.hexagonContainer.position.y > this.hexagonContainer.height / 2 + 5 || this.hexagonContainer.position.y < -this.hexagonContainer.height / 2 - 5) {
+                this.isHovered = false;
+            }
+
+            this.isAnimated = false;
+        } else if(!this.isHovered) {
+            this.isAnimated = true;
+            TweenMax.to(this.hexagonContainer.scale, 1, { x: 1, y: 1, ease: Power2.easeOut });
+            TweenMax.to(this.hexagonContainer.position, 1, { x: 0, y: 0, ease: Power2.easeOut });
+            
+            
+            if(!this.isAnimated) {
+                for (let stroke of this.strokes) {
+                    stroke.clear();
+                }
+
+                this.drawBars.bind(this)();
+            }
+            
+            this.isAnimated = false;
         }
     }
 }
